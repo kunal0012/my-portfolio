2933f3bca1b4fcd640d539cd63f0f8e2e4431ac6
kunal0012 <118892601+kunal0012@users.noreply.github.com>
2026-01-20 13:41:50 +0530

    Delete duplicate directory

--- my-portfolio/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-6 text-gray-500 border-t border-gray-800">
-      © {new Date().getFullYear()} Kunal Bhardwaj
-    </footer>
-  );
-}
